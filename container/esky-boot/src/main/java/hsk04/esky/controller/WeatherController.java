2db2e0cb645cc99fb4a37e588d083b616d433d66
Kishore Hulikere Sampathkumar <hskishore4@gmail.com>
2018-05-21 02:33:15 -0600

    cors for all clients

--- container/esky-boot/src/main/java/hsk04/esky/controller/WeatherController.java
+++ container/esky-boot/src/main/java/hsk04/esky/controller/WeatherController.java
@@ -18,7 +18,7 @@ import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = URI.WEATHER)
-@CrossOrigin(origins="http://mocker.egen.io")
+@CrossOrigin(origins="*")
 public class WeatherController {
 
 	private WeatherService service;
